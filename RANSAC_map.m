5c8b58e5662dd21a26d10d1b79094219a4380f86
Sam Daitzman <sdaitzman@olin.edu>
2019-04-30 23:30:45 -0400

    refactor

--- RANSAC_map.m
+++ RANSAC_map.m
@@ -3,7 +3,7 @@ clear;
 load('allRoomDataCleaned.mat');
 x = data(1,:)';
 y = data(2,:)';
-p = polyfit(x, y, 1)
+p = polyfit(x, y, 1);
 yp = polyval(p, x);
 figure(1);clf;
 plot(x,y, 'o');hold on;
@@ -15,14 +15,14 @@ ylabel('y position (meters)');
 
 
 coefficients = pca([x y]);
-m = coefficients(2,1) / coefficients(1,1)
-b = mean(y) - m * mean(x)
+m = coefficients(2,1) / coefficients(1,1);
+b = mean(y) - m * mean(x);
 pcay = polyval([m b], x);
 plot(x, pcay, 'r')
 
 clearvars -except x y
 
-[bestTestLine] = RANSAC(x,y,0.05,100)
+[bestTestLine] = RANSAC(x,y,0.05,100);
 
 plot(x, bestTestLine, 'r*')
 
@@ -44,15 +44,12 @@ bestTestLineSoFar = [];
 for i = 1:n
     matchedPoints = [];
     % Randomly select the points
-    point1 = points(randi([1, length(points)]),:);
-    point2 = points(randi([1, length(points)]),:);
+    A = points(randi([1, length(points)]),:);
+    B = points(randi([1, length(points)]),:);
     
     % Find and plot the line between them
-    p = polyfit([point1(1) point2(1)], [point1(2) point2(2)], 1);
+    p = polyfit([A(1) B(1)], [A(2) B(2)], 1);
     testline = polyval(p, x);
-    
-    A = point1;
-    B = point2;
     
     That = [B(1) - A(1);B(2) - A(2);0]./vecnorm([B(1) - A(1);B(2) - A(2);0]);
     Khat = [0;0;1];
@@ -63,12 +60,12 @@ for i = 1:n
     % Test every point with this fit line
     for j = 1:length(points)
 
-        r = points(j,:) - point1;
+        r = points(j,:) - A;
         r(3) = 0;
         
-        a = point1 - point2;
+        a = A - B;
         a(3) = 0;
-        b = points(j,:) - point2;
+        b = points(j,:) - B;
         b(3) = 0;
 %         dist = norm(cross(a,b)) / norm(a);
         
@@ -81,9 +78,8 @@ for i = 1:n
     end
     
     if inliers > bestInliersSoFar
-        bestMatchedPoints = matchedPoints;
-        bestPoint1SoFar = point1;
-        bestPoint2SoFar = point2;
+        bestPoint1SoFar = A;
+        bestPoint2SoFar = B;
         bestTestLineSoFar = testline;
         bestInliersSoFar = inliers;
         disp(inliers)
