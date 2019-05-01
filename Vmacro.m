93e26df4d23987346deecdb3eccaaa42a4c89037
Dieter Brehm <inkering@gmail.com>
2019-05-01 13:11:25 -0400

    plots exports and implementation of lines

--- Vmacro.m
+++ Vmacro.m
@@ -13,23 +13,23 @@ for i=1:xlim
         %should be able to figure out what this is and edit appropriately to get
         %what you want
 
-        line = @(x0,m,xi,yi) sqrt((px(i,j)-x0-xi).^2 + ((py(i,j)-(m.*x0)-yi).^2));
-        round = @(xi,yi) sqrt((px(i,j)-xi).^2 + ((py(i,j)-yi).^2));
-        dV = @(x0) line(x0, 1,0,0) +...
-                   line(x0, 1,2,0) +...
-                   line(x0, -1,0,2) +...
-                   line(x0, -1,0,2);
-        dV2 = @(x0)line(x0, 0.1,0,0);
-        dV3 = @(x0)line(x0, 1,0,0);
-        dV4 = @(x0)line(x0, 100,2,1.25);
-        V(i,j) = -0.1*integral(dV3,-2,2); %- integral(dV4,-2,2); %+ integral(dV3,0,2.5);% + 0.1*round(1,1.25);
+        line = @(x0,m,xi,yi) log(sqrt((px(i,j)-x0-xi).^2 + ((py(i,j)-(m.*x0)-yi).^2)));
+        round = @(xi,yi) log(sqrt((px(i,j)-xi).^2 + ((py(i,j)-yi).^2)));
+        dV2 = @(x0)line(x0, 0.01,1,0);
+        dV3 = @(x0)line(x0, 1,1.75,2.25);
+        dV4 = @(x0)line(x0, -1,1.5,1);
+        dV5 = @(x0)line(x0, 1,0.5,1.5);
+%         V(i,j) = integral(dV3,-2,2) + integral(dV4,-2,2);% + integral(dV2,-2,2);% + 0.1*round(1,1.25);
+%         V(i,j) = -integral(dV3,-2,2) + -integral(dV2,-2,2);
+        V(i,j) = 2*integral(dV5,0.2,-0.2) + 2*integral(dV4,0.2,-0.2) + -2*integral(dV3,0.2,-0.2) + 0.5*integral(dV2,1,-1);
     end
 end
 figure()
 hold off
-%contour3(px,py,V)
-surf(px,py,V)
+contour3(px,py,V)
+%surf(px,py,V)
 [Ex,Ey] = gradient(V);
 hold on
 axis equal
-%quiver(px,py,-Ex,-Ey)
+colorbar
+quiver(px,py,-Ex,-Ey)
