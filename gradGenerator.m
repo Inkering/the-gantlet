459506459d79703ab727bb57ea95cc04dd409003
Dieter Brehm <inkering@gmail.com>
2019-05-01 19:46:38 -0400

    hey looks its gradient descent

--- gradGenerator.m
+++ gradGenerator.m
@@ -9,20 +9,15 @@ V = zeros(xlim, ylim);
 % loop through all coords
 for i=1:xlim
     for j=1:ylim
-        % define equation of a line as function
-        line = @(x0,m,xi,yi) log(sqrt((px(i,j)-x0-xi).^2 + ((py(i,j)-(m.*x0)-yi).^2)));
-        
-        % define equation of a circle
-%         round = @(xi,yi) log(sqrt((px(i,j)-xi).^2 + ((py(i,j)-yi).^2)));
-        
-        % define potentials for some lines
-        dV2 = @(x0)line(x0, 0.01,1,0);
-        dV3 = @(x0)line(x0, 1,1.75,2.25);
-        dV4 = @(x0)line(x0, -1,1.5,1);
-        dV5 = @(x0)line(x0, 1,0.5,1.5);
-        
-        % build potential at that point
-        V(i,j) = 2*integral(dV5,0.2,-0.2) + 2*integral(dV4,0.2,-0.2) + -2*integral(dV3,0.2,-0.2) + 0.5*integral(dV2,1,-1);
+          line = @(x0,m,xi,yi) log(sqrt(( px(i,j)-x0-xi).^2 + ((py(i,j)-(m.*x0)-yi).^2)));
+          circle = @(xi,yi) log(sqrt((px(i,j)-xi).^2 + ((py(i,j)-yi).^2)));
+          dV4 = @(x0)line(x0, -1,1.5,1);
+          dV5 = @(x0)line(x0, 1,0.5,1.5);
+          dV3 = @(x0)line(x0, 0.01,0,0);
+          dV6 = @(x0)line(x0, 0.01,0,2.5);
+          dV7 = @(x0)line(x0, -1,0,0);
+          dV8 = @(x0)line(x0, 1000,2,0);
+          V(i,j) = integral(dV5,0.2,-0.2) + 2*integral(dV4,0.2,-0.2) + integral(dV3,4,-4) + integral(dV6,4,-4) + integral(dV7,4,-4)./3 + 6*integral(dV8,4,-4) + circle(1.75,2.25);
     end
 end
 
